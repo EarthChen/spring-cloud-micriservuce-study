9b67915bfe206bfda57afd31b2cd55b8fd19c37b
EarthChen <179153809@qq.com>
2017-09-06 23:38:47 +0800

    获取配置中心的客户端config-client

--- microservice-config-client/src/main/java/com/earthchen/ConfigClientApplication.java
+++ microservice-config-client/src/main/java/com/earthchen/ConfigClientApplication.java
@@ -1,4 +1,20 @@
 package com.earthchen;
 
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * config client
+ * 获取配置信息
+ */
+@SpringBootApplication
 public class ConfigClientApplication {
+
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigClientApplication.class)
+                .web(true)
+                .run(args);
+    }
 }
