26a531d7fcbaacf7f4eb048c1631e70bd4659d4e
EarthChen <179153809@qq.com>
2017-09-10 21:34:17 +0800

    ribbon hystrix熔断器

--- microservice-eureka-consumer-ribbon-hystrix/src/main/java/com/earthchen/EurekaConsumerRibbonHystrixApplcation.java
+++ microservice-eureka-consumer-ribbon-hystrix/src/main/java/com/earthchen/EurekaConsumerRibbonHystrixApplcation.java
@@ -1,4 +1,35 @@
 package com.earthchen;
 
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+/**
+ * 使用@EnableCircuitBreaker @EnableHystrix注解开启Hystrix的使用
+ */
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@SpringBootApplication
 public class EurekaConsumerRibbonHystrixApplcation {
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaConsumerRibbonHystrixApplcation.class)
+                .web(true)
+                .run(args);
+    }
 }
--- microservice-eureka-consumer-ribbon-hystrix/src/main/java/com/earthchen/web/DcController.java
+++ microservice-eureka-consumer-ribbon-hystrix/src/main/java/com/earthchen/web/DcController.java
@@ -1,4 +1,23 @@
 package com.earthchen.web;
 
+import com.earthchen.service.ConsumerService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
 public class DcController {
+
+    @Autowired
+    ConsumerService consumerService;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return consumerService.consumer();
+    }
+
+
 }
