239764b448d42c7c59ff4d0d0aa004eb3af6cbc7
EarthChen <179153809@qq.com>
2017-09-10 16:26:34 +0800

    通过消息代理收集聚合

--- turbine-amqp/src/main/java/com/earthchen/TurbineApplication.java
+++ turbine-amqp/src/main/java/com/earthchen/TurbineApplication.java
@@ -1,4 +1,24 @@
 package com.earthchen;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * 基于消息代理的Turbine聚合服务
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableTurbineStream
+@EnableDiscoveryClient
 public class TurbineApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class, args);
+    }
 }
